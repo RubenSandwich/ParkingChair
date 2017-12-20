37c59e45c40368f8ab065e8e6fa18bd69092dca9
Ruben Niculcea <ruben.niculcea@gmail.com>
2017-12-19 19:50:15 -0500

    Add keys required to hit endpoints

--- Client/src/index.ts
+++ Client/src/index.ts
@@ -1,1 +1,22 @@
-console.log("test");
+import * as dotenv from "dotenv";
+dotenv.config();
+
+declare const process: {
+  env: {
+    API_KEY: string;
+  };
+};
+
+import fetch from "node-fetch";
+
+const url =
+  "https://m3hk0nej61.execute-api.us-east-1.amazonaws.com/dev/sendText";
+
+fetch(url, {
+  method: "POST",
+  headers: {
+    "X-Api-Key": process.env.API_KEY
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json));
